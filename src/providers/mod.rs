a8745615658b9627226dc235522551698e97d55d
binhminhanh1235 <binhminhanh1235@gmail.com>
2026-09-16 22:18:08 +0700

    feat: expose OmniVoice provider client

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,6 +1,11 @@
+mod omnivoice;
 mod pexels;
 mod stock;
 
+pub use omnivoice::{
+    GenerateProjectOptions, OmniVoiceClient, OmniVoiceConnection, OmniVoiceError,
+    OmniVoiceImportResult, OmniVoiceJobSubmission, OmniVoiceRemoteJob,
+};
 pub use pexels::PexelsProvider;
 pub use stock::{
     AssetKind, CreatorAttribution, ProviderConnection, RateLimitMetadata, StockAssetCandidate,
