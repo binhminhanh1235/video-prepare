5d7ea77542db21fffadf90d5d433255b7bc2acee
binhminhanh1235 <binhminhanh1235@gmail.com>
2026-09-16 22:32:08 +0700

    feat: expose audio artifact completion proof

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod audio;
+pub mod audio_artifacts;
 pub mod project;
 pub mod providers;
 pub mod script;
@@ -9,15 +10,22 @@ pub use audio::{
     AudioAttemptStatus, AudioError, AudioExecutor, AudioFlowStatus, AudioGenerationSettings,
     AudioRunSummary, AUDIO_STATUS_SCHEMA_VERSION,
 };
+pub use audio_artifacts::{
+    audio_artifact_proof_path, load_audio_artifact_proof, reconcile_local_audio_artifact,
+    sync_latest_audio_artifact, AudioArtifactError, AudioArtifactProof, AudioArtifactSyncSummary,
+    AUDIO_ARTIFACT_SCHEMA_VERSION,
+};
 pub use project::{
     ProjectError, ProjectMetadata, ProjectStatus, ProjectStore, SceneRuntimeStatus, StoredProject,
     TaskState, PROJECT_SCHEMA_VERSION, STATUS_SCHEMA_VERSION,
 };
 pub use providers::{
-    AssetKind, CreatorAttribution, GenerateProjectOptions, OmniVoiceClient, OmniVoiceConnection,
-    OmniVoiceError, OmniVoiceImportResult, OmniVoiceJobSubmission, OmniVoiceProvider,
-    OmniVoiceRemoteJob, PexelsProvider, ProviderConnection, RateLimitMetadata, StockAssetCandidate,
-    StockProvider, StockProviderError, StockRendition, StockSearchPage, StockSearchRequest,
+    AssetKind, CreatorAttribution, GenerateProjectOptions, OmniVoiceArtifact,
+    OmniVoiceArtifactDownload, OmniVoiceArtifactProvider, OmniVoiceArtifactTransport,
+    OmniVoiceClient, OmniVoiceConnection, OmniVoiceError, OmniVoiceImportResult,
+    OmniVoiceJobSubmission, OmniVoiceProvider, OmniVoiceRemoteJob, PexelsProvider,
+    ProviderConnection, RateLimitMetadata, StockAssetCandidate, StockProvider, StockProviderError,
+    StockRendition, StockSearchPage, StockSearchRequest,
 };
 pub use script::{
     parse_script, MediaKind, OmniVoiceScript, OmniVoiceSection, PreparedScript, SceneSpec,
