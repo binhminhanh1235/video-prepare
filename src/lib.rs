606b72eb8dda1ece48121189e9004f45e01d6e31
binhminhanh1235 <binhminhanh1235@gmail.com>
2026-09-16 20:33:31 +0700

    feat: expose stock provider API

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 pub mod project;
+pub mod providers;
 pub mod script;
 
 pub use project::{
     ProjectError, ProjectMetadata, ProjectStatus, ProjectStore, SceneRuntimeStatus, StoredProject,
     TaskState, PROJECT_SCHEMA_VERSION, STATUS_SCHEMA_VERSION,
+};
+pub use providers::{
+    AssetKind, CreatorAttribution, PexelsProvider, ProviderConnection, RateLimitMetadata,
+    StockAssetCandidate, StockProvider, StockProviderError, StockRendition, StockSearchPage,
+    StockSearchRequest,
 };
 pub use script::{
     parse_script, MediaKind, OmniVoiceScript, OmniVoiceSection, PreparedScript, SceneSpec,
