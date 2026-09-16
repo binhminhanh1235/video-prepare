09cb01b68bb958facad1e59d3993f8e908dc7123
binhminhanh1235 <binhminhanh1235@gmail.com>
2026-09-16 22:27:29 +0700

    feat: expose OmniVoice artifact transport

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,4 +1,5 @@
 mod omnivoice;
+mod omnivoice_artifacts;
 mod omnivoice_gateway;
 mod pexels;
 mod stock;
@@ -6,6 +7,10 @@ mod stock;
 pub use omnivoice::{
     GenerateProjectOptions, OmniVoiceClient, OmniVoiceConnection, OmniVoiceError,
     OmniVoiceImportResult, OmniVoiceJobSubmission, OmniVoiceRemoteJob,
+};
+pub use omnivoice_artifacts::{
+    safe_local_filename, verify_local_artifact, OmniVoiceArtifact, OmniVoiceArtifactDownload,
+    OmniVoiceArtifactProvider, OmniVoiceArtifactTransport,
 };
 pub use omnivoice_gateway::OmniVoiceProvider;
 pub use pexels::PexelsProvider;
